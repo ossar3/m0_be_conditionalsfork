b99bee94dd57889c9f34fc2397e43e851edcabe7
Oscar Patterson <oepatterson12@gmail.com>
2024-08-08 14:30:55 -0400

    thursday

--- comparisons.rb
+++ comparisons.rb
@@ -22,24 +22,31 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+p "is number_teachers less than number_students?", number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+p "number_teachers equal to string_teachers?", number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+p "is number_teachers not equal to number_students?", number_teachers != number_students
 # this should print: true
 
-# YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+# YOU DO: print the result of the comparison: 
+p "is number_students greater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+p "is number_students greater than or equal to 21?", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+p "is number_students less than or equal to 20?", number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+p "is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
